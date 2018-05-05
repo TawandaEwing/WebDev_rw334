94c58fde2c917f14814a474252f48089b0251abc
Marthinus Bosman <18955185@sun.ac.za>
2018-05-05 13:34:09 +0200

    setup 0.02

--- site/__init__.py
+++ site/__init__.py
@@ -1,6 +1,12 @@
 from .views import app
 from .models import graph
 
-graph.schema.create_uniqueness_constraint("User", "username")
-graph.schema.create_uniqueness_constraint("Tag", "name")
-graph.schema.create_uniqueness_constraint("Post", "id")
+def create_uniqueness_constraint(label, property):
+    query = "CREATE CONSTRAINT ON (n:{label}) ASSERT n.{property} IS UNIQUE"
+    query = query.format(label=label, property=property)
+    graph.cypher.execute(query)
+
+create_uniqueness_constraint("User", "username")
+create_uniqueness_constraint("Topic", "name")
+create_uniqueness_constraint("Question", "id")
+create_uniqueness_constraint("Answer", "id")
