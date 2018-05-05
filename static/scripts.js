dc41585bdff4916cea893749c6fe349536e1dc92
Emile Lombard <13236814+EmileLombard@users.noreply.github.com>
2018-05-05 17:51:00 +0200

    Commented out - will use for search etc.
    Will write search function etc. soon

--- static/scripts.js
+++ static/scripts.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
 
-	$('form').on('submit', function(event) {
-
-		$.ajax({
-			data : {
-				name : $('#nameInput').val(),
-				email : $('#emailInput').val()
-			},
-			type : 'POST',
-			url : '/process'
-		})
-		
-		//.done is wanneer die ajax call complete is. 
-		.done(function(data) {
-
-			if (data.error) {
-				$('#errorAlert').text(data.error).show();
-				$('#successAlert').hide();
-			}
-			else {
-				$('#successAlert').text(data.name).show();
-				$('#errorAlert').hide();
-			}
-
-		});
-
-		event.preventDefault(); //prevent HTML from submitting the data and force it to use jQuery as defined here
-
-	});
+	//$('form').on('submit', function(event) {
+	//
+	//	$.ajax({
+	//		data : {
+	//			name : $('#nameInput').val(),
+	//			email : $('#emailInput').val()
+	//		},
+	//		type : 'POST',
+	//		url : '/process'
+	//	})
+	//	
+	//	//.done is wanneer die ajax call complete is. 
+	//	.done(function(data) {
+	//
+	//		if (data.error) {
+	//			$('#errorAlert').text(data.error).show();
+	//			$('#successAlert').hide();
+	//		}
+	//		else {
+	//			$('#successAlert').text(data.name).show();
+	//			$('#errorAlert').hide();
+	//		}
+	//
+	//	});
+	//
+	//	event.preventDefault(); //prevent HTML from submitting the data and force it to use jQuery as defined here
+	//
+	//});
 
 });
