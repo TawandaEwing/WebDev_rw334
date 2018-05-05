55486f55b8acf50f57c882f27a5b95eb64858c49
Marthinus Bosman <18955185@sun.ac.za>
2018-05-05 13:44:28 +0200

    setup 0.03

--- site/views.py
+++ site/views.py
@@ -9,13 +9,14 @@ def register():
 		email = request.form['email']
         username = request.form['username']
         password = request.form['psw']
-
-        if len(username) < 1:
+		if len(email) < 1:																# Proper email checks
+			flash('Your email must be at least one character.')
+        if len(username) < 1:															# better username checks?
             flash('Your username must be at least one character.')
-        elif len(password) < 5:
+        elif len(password) < 5:															# better password checks?
             flash('Your password must be at least 5 characters.')
-        elif not User(username).register(password):
-            flash('A user with that username already exists.')
+        elif not User(username).register(email, password):								# Should probably have separate checks for username and email
+            flash('A user with that username or email already exists.')
         else:
             session['username'] = username
             flash('Logged in.')
